3223e145c921fa8c579d71d34048bfeee9623d1d
Reuben Jefwa <78743046+reubendeekay@users.noreply.github.com>
2021-10-23 00:59:59 +0300

    Update call_to_action_widget.dart

--- lib/utilities/widgets/onboarding_screen/call_to_action_widget.dart
+++ lib/utilities/widgets/onboarding_screen/call_to_action_widget.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 
 Widget callToAction({text = 'Get Started', homeRoute, context}) {
   return Container(
-    height: 70,
+    height: 50,
     width: double.infinity,
-    color: Colors.white,
+    margin: EdgeInsets.fromLTRB(25, 0, 25, 20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(25),
+      color: Colors.white,
+    ),
     child: GestureDetector(
       onTap: () {
         try {
           Navigator.pushNamed(context, homeRoute);
         } catch (e) {
           print(e);
-          print("Set homeRoute to the route where you want to land after on-boarding");
+          print(
+              "Set homeRoute to the route where you want to land after on-boarding");
         }
       },
       child: Center(
         child: Padding(
-          padding: EdgeInsets.only(bottom: 10),
+          padding: EdgeInsets.all(5),
           child: Text(
             text,
             style: TextStyle(
